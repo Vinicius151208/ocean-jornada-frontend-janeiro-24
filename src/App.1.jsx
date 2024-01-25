76832249bd9867d9f746e97268d78c5d66d3b3e3
Vinicius151208 <143668303+Vinicius151208@users.noreply.github.com>
2024-01-25 13:40:57 +0000

    Criação do elemento HTML

---
+++ src/App.1.jsx
@@ -0,0 +1,11 @@
+export function App() {
+  return (
+    <>
+      <div>
+        <h2>Rick Sanchez</h2>
+        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+          App />
+      </div>*
+    </>
+  );
+}
