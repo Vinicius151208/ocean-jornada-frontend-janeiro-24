c727549c89c87c2d61d4898caee7172f02a39663
Vinicius151208 <143668303+Vinicius151208@users.noreply.github.com>
2024-01-30 15:09:17 +0000

    Criação do tem 4

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,38 @@ import Card from './components/Card/Card'
 
 function App() {
   const item1 = {
-    name: 'Rick Sanchez' ,
+    name: 'Rick Sanchez',
     image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
   }
 
   const item2 = {
-    name: 'Morty Smith' ,
+    name: 'Morty Smith',
     image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
   }
 
   const item3 = {
-    name: 'Summer Smith' ,
+    name: 'Summer Smith',
     image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
   }
+
+  const item4 = {
+    name: 'Beth Smith',
+    image: 'https://rickandmortyapi.com/api/character/avatar/4.jpeg'
+  }
+
+  const itens = [item1, item2, item3, item4]
+
   return (
     <>
-    <Card item={item1} />
-    <Card item={item1} />
+      <div className="cards">
+        {/* <Card item={item1} />
+        <Card item={item2} />
+        <Card item={item3} /> */}
+        {itens.map(item => <Card item={item} />)}
+      </div>
     </>
   )
 }
 
 export default App
+
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,24 +3,23 @@ function Card(props) {
 
   const item = props.item
 
-  console.log(item)
+  //console.log(item)
 
-    return (
-        <>
-          <div className="card">
-            <h2>{item.name}z</h2>
+  return (
+    <>
+      <div className="card">
+        <h2>{item.name}</h2>
 
-            <div className="tags">
-            <div className="tag">Status: Vivo</div>
-            <div className="tag">Espécie: Humana</div>
-            <div className="tag">Origem: Terra C-137</div>
-            </div>
+        <div className="tags">
+          <div className="tag">Status: Vivo</div>
+          <div className="tag">Espécie: Humana</div>
+          <div className="tag">Origem: Terra C-137</div>
+        </div>
 
-
-            <img src={item.image} />
-          </div>
-        </>
-      )
+        <img src={item.image} />
+      </div>
+    </>
+  )
 }
 
 export default Card
