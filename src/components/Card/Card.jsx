140b4c9a409d4d8ece438765f71b51dda9f384bc
Vinicius151208 <143668303+Vinicius151208@users.noreply.github.com>
2024-01-30 13:37:17 +0000

    Configurando cards

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,13 +1,26 @@
-function Card() {
+function Card(props) {
+  //console.log(props)
+
+  const item = props.item
+
+  console.log(item)
+
     return (
         <>
           <div className="card">
-            <h2>Rick Sanchez</h2>
-            <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+            <h2>{item.name}z</h2>
+
+            <div className="tags">
+            <div className="tag">Status: Vivo</div>
+            <div className="tag">Espécie: Humana</div>
+            <div className="tag">Origem: Terra C-137</div>
+            </div>
+
+
+            <img src={item.image} />
           </div>
         </>
       )
 }
-
 
 export default Card
